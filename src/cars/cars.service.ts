df2bba3ff04e83211de258779f0c7d76163eb5e4
langat68 <langatfidel3@gmail.com>
2025-06-24 01:27:04 +0300

    filterd the response from db

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -2,14 +2,53 @@
 import { db } from '../db/db.js'
 import { cars } from '../db/schema.js'
 import { eq } from 'drizzle-orm'
+import { sql } from 'drizzle-orm'
 
 export class CarService {
   async getAllCars() {
-    return db.select().from(cars)
+    return db
+      .select({
+        id: cars.id,
+        make: cars.make,
+        model: cars.model,
+        carName: sql<string>`${cars.make} || ' ' || ${cars.model}`.as('carName'),
+        year: cars.year,
+        category: cars.category,
+        pricePerHour: cars.pricePerHour,
+        pricePerDay: cars.pricePerDay,
+        fuel: cars.fuel,
+        transmission: cars.transmission,
+        capacity: cars.capacity,
+        availability: cars.availability,
+        location: cars.location,
+        createdBy: cars.createdBy,
+        createdAt: cars.createdAt,
+      })
+      .from(cars)
   }
 
   async getCarById(id: number) {
-    return db.select().from(cars).where(eq(cars.id, id)).then(res => res[0])
+    return db
+      .select({
+        id: cars.id,
+        make: cars.make,
+        model: cars.model,
+        carName: sql<string>`${cars.make} || ' ' || ${cars.model}`.as('carName'),
+        year: cars.year,
+        category: cars.category,
+        pricePerHour: cars.pricePerHour,
+        pricePerDay: cars.pricePerDay,
+        fuel: cars.fuel,
+        transmission: cars.transmission,
+        capacity: cars.capacity,
+        availability: cars.availability,
+        location: cars.location,
+        createdBy: cars.createdBy,
+        createdAt: cars.createdAt,
+      })
+      .from(cars)
+      .where(eq(cars.id, id))
+      .then(res => res[0])
   }
 
   async createCar(data: Omit<typeof cars.$inferInsert, 'id'>) {
